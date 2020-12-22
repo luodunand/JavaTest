1d0dbd419229edb2bce5fea464ed5ef735c63538
admin <294350899@qq.com>
2020-12-22 20:45:07 +0800

    test

---
+++ src/animal/Test.java
@@ -0,0 +1,17 @@
+package animal;
+
+public class Test {
+    public static void main(String[] args){
+        Cat a = new Cat();
+        a.setName("a");
+        Cat b = new Cat("Bob");
+        Fish c = new Fish();
+        c.setName("c");
+        Fish d = new Fish("D");
+        Spider e = new Spider();
+
+        System.out.print(a.getName()+a.legs+"\n"+b.getName()+b.legs+"\n"+c.getName()+c.legs+"\n"+d.getName()+d.legs+"\n"+e.legs);
+    }
+}
+
+

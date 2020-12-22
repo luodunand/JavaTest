dce765c1d3937b3ffc0c9050ddf2b3497d3b9fe6
admin <294350899@qq.com>
2020-12-22 20:48:08 +0800

    test

--- src/animal/Test.java
+++ src/animal/Test.java
@@ -7,7 +7,7 @@ public class Test {
         Cat b = new Cat("Bob");
         Fish c = new Fish();
         c.setName("c");
-        Fish d = new Fish("D");
+        Fish d = new Fish("DDD");
         Spider e = new Spider();
 
         System.out.print(a.getName()+a.legs+"\n"+b.getName()+b.legs+"\n"+c.getName()+c.legs+"\n"+d.getName()+d.legs+"\n"+e.legs);
